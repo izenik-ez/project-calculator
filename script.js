ef155c7fbf3cf17e8a640cab7eb05dc82e0d5de4
Izenikez <62757324+izenik-ez@users.noreply.github.com>
2022-11-18 21:56:38 +0100

    Basic functionality of calculator

--- script.js
+++ script.js
@@ -1,58 +1,81 @@
 let operand = 0;
 let operator = "";
 
+
+let calculator = {
+    operand : 0,
+    operator : "",
+    clear: function(mantisa){
+        this.operand = 0;
+        this.operator = "";
+        mantisa.innerHTML = 0;
+    },
+    operation: function(operator){
+        this.operator = operator
+    },
+    calculate: function(number){
+        console.log("Operand: " + this.operand);
+        console.log("Operator: " + this.operator);
+        switch(this.operator){
+        case "+":
+            this.operand = this.operand + number;
+            break;                    
+        case "-":
+            this.operand = this.operand - number;
+            break;
+        case "*":
+            this.operand = this.operand * number;
+            break;
+        case "/":
+            this.operand = this.operand / number;
+            break;
+        }
+        return this.operand;
+    }
+};
+
 let app = function (){
-    let calculator = document.querySelector(".calculator");
     let mantisa = document.querySelector(".mantisa");
     let operands = document.querySelectorAll(".operand");
+
+    document.querySelector(".clear").addEventListener("click", (e) =>{
+        calculator.clear(mantisa);
+    });
+    
     for(let i = 0 ; i < operands.length ; i++)
     {
         operands[i].addEventListener("click", (e) => {
-            if(mantisa.innerHTML === 0){
+            if(+mantisa.innerHTML === 0){
                 mantisa.innerHTML = e.target.value;
             }else{
                 mantisa.innerHTML = mantisa.innerHTML + e.target.value;
             }
+
         });
     }
     let operators = document.querySelectorAll(".operator");
     operators.forEach((op) => {
         op.addEventListener("click", (e) => {
-            if(operator === "" ){
-                operator = e.target.value;
-                operand = +mantisa.innerHTML;
-                mantisa.innerHTML  = 0;
-            } else {
-                switch(e.target.value){
-                case "+":
-                    mantisa.innerHTML = operand + +mantisa.innerHTML;
-                    break;                    
-                    case "-":
-                    mantisa.innerHTML = operand - +mantisa.innerHTML;
-                    break;
-                    case "*":
-                    mantisa.innerHTML = operand * +mantisa.innerHTML;
-                    break;
-                    case "/":
-                    mantisa.innerHTML = operand / +mantisa.innerHTML;
-                    break;
-                }
-                
+            calculator.operator = e.target.value;
+            calculator.operand = +mantisa.innerHTML;
+            mantisa.innerHTML = 0;
+            /*
+            if(calculator.operation !== ""){
+                mantisa.innerHTML = calculator.calculate(+mantisa.innerHTML);
+            }else{
+                calculator.operand = +mantisa.innerHTML;
+                mantisa.innerHTML = 0;
             }
+            calculator.operator = e.target.value;
+*/
         });
     });
-    
-    calculator.addEventListener("click", (e) =>{
-        console.log(e.target.className);        
-        if(e.target.className === "operator"){
-            if( operand === 0 ){
-                operand = +mantisa.innerHTML;
-            } else {
-                operand = operand + +mantisa.innerHTML;
-            }           
-        }
-        console.log(operand);
-    });    
+    let equalKey = document.querySelector(".equals");
+    equalKey.addEventListener("click", (e) =>{
+        mantisa.innerHTML = calculator.calculate(+mantisa.innerHTML);
+        calculator.operand = 0;
+        calculator.operator = "";
+    });   
 }
 
 window.onload = function() {app();}
